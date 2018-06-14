e68e1d10cc09435374fd234305f5ec8dfb07998a
Jiahe-J <jjh9401@qq.com>
2018-06-14 12:38:17 +0800

    修改ReportDemoView

--- report/frontend_views.py
+++ report/frontend_views.py
@@ -1,9 +1,10 @@
 # coding=utf8
-
-
+from django.http import JsonResponse
 from django.views.generic.base import TemplateView
 from django_echarts.views.frontend import EChartsFrontView
+from pyecharts import Bar
 
+from utils.data_parser import deal_in_time_rate_parser
 from .demo_data import FACTORY
 
 
@@ -21,12 +22,57 @@ class ReportDemoEchartsTemplate(TemplateView):
 
 class SimpleBarView(EChartsFrontView):
     def get_echarts_instance(self, **kwargs):
-        return FACTORY.create('bar')
+        return FACTORY.create('bar', **kwargs)
 
 
+# 获取前端参数,处理,返回json
 class ReportDemoView(EChartsFrontView):
     def get_echarts_instance(self, **kwargs):
-        return FACTORY.create('reportBar')
+        year = kwargs.get('year')
+        quarter = kwargs.get('quarter')
+        print(year, quarter)
+        print(kwargs.get('year'))
+        bar = Bar("故障处理及时率", "珠三角地区")
+        cites, rate = deal_in_time_rate_parser()
+        bar.add("珠三角地区1", cites, rate)
+        bar.renderer = 'svg'
+        dt = {'title': [{'text': '故障处理及时率', 'subtext': '珠三角地区', 'left': 'auto', 'top': 'auto', 'textStyle': {'color': '#000', 'fontSize': 18},
+                         'subtextStyle': {'color': '#aaa', 'fontSize': 12}}],
+              'toolbox': {'show': True, 'orient': 'vertical', 'left': '95%', 'top': 'center',
+                          'feature': {'saveAsImage': {'show': True, 'title': '下载图片'}, 'restore': {'show': True}, 'dataView': {'show': True}}},
+              'series_id': 2946983, 'tooltip': {'trigger': 'item', 'triggerOn': 'mousemove|click', 'axisPointer': {'type': 'line'}, 'formatter': None,
+                                                'textStyle': {'color': '#fff', 'fontSize': 14}, 'backgroundColor': 'rgba(50,50,50,0.7)',
+                                                'borderColor': '#333', 'borderWidth': 0}, 'series': [
+                {'type': 'bar', 'name': '珠三角地区', 'data': [100, 99, 98, 97, 95, 88, 87, 85], 'stack': '', 'barCategoryGap': '20%',
+                 'label': {'normal': {'show': False, 'position': 'top', 'textStyle': {'color': '#000', 'fontSize': 12}, 'formatter': None},
+                           'emphasis': {'show': True, 'position': None, 'textStyle': {'color': '#fff', 'fontSize': 12}}}, 'markPoint': {'data': []},
+                 'markLine': {'data': []}, 'seriesId': 2946983}], 'legend': [
+                {'data': ['珠三角地区'], 'selectedMode': 'multiple', 'show': True, 'left': 'center', 'top': 'top', 'orient': 'horizontal',
+                 'textStyle': {'fontSize': 12, 'color': '#333'}}], 'backgroundColor': '#fff', 'xAxis': [
+                {'name': '', 'show': True, 'nameLocation': 'middle', 'nameGap': 25, 'nameTextStyle': {'fontSize': 14},
+                 'axisLabel': {'interval': 'auto', 'rotate': 0, 'margin': 8, 'textStyle': {'fontSize': 12, 'color': '#000'}},
+                 'axisTick': {'alignWithLabel': False}, 'inverse': False, 'position': None, 'boundaryGap': True, 'min': None, 'max': None,
+                 'data': ['佛山', '广州', '深圳', '东莞', '中山', '珠海', '惠州', '江门'], 'type': 'category'}], 'yAxis': [
+                {'name': '', 'show': True, 'nameLocation': 'middle', 'nameGap': 25, 'nameTextStyle': {'fontSize': 14},
+                 'axisLabel': {'formatter': '{value} ', 'rotate': 0, 'interval': 'auto', 'margin': 8, 'textStyle': {'fontSize': 12, 'color': '#000'}},
+                 'axisTick': {'alignWithLabel': False}, 'inverse': False, 'position': None, 'boundaryGap': True, 'min': None, 'max': None,
+                 'splitLine': {'show': True}, 'type': 'value'}],
+              'color': ['#c23531', '#2f4554', '#61a0a8', '#d48265', '#749f83', '#ca8622', '#bda29a', '#6e7074', '#546570', '#c4ccd3', '#f05b72',
+                        '#ef5b9c', '#f47920', '#905a3d', '#fab27b', '#2a5caa', '#444693', '#726930', '#b2d235', '#6d8346', '#ac6767', '#1d953f',
+                        '#6950a1', '#918597', '#f6f5ec']}
+
+        return dt  # 相当于bar.options
+
+    def get(self, request, **kwargs):
+        year = request.GET.get('year', '')
+        quarter = request.GET.get('quarter', '')
+        kwargs.__setitem__('year', year)
+        kwargs.__setitem__('quarter', quarter)
+        echarts_instance = self.get_echarts_instance(**kwargs)
+        # print(year, quarter)
+        # print(kwargs.get('year'))
+        print(echarts_instance)
+        return JsonResponse(data=echarts_instance, safe=False)
 
 
 class SimpleKLineView(EChartsFrontView):
