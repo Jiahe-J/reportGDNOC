381c060c377cfe7a2f753db8cfb183003744d29d
jiangjiahe <jiangjiahe.gd@chinatelecom.cn>
2018-07-19 23:05:28 +0800

    修改doc/sql

--- doc/sql/DjangoEcharts_report_district.sql
+++ doc/sql/DjangoEcharts_report_district.sql
@@ -1,5 +1,5 @@
-INSERT INTO DjangoEcharts.report_district (id, district) VALUES (1, '珠三角1地区');
-INSERT INTO DjangoEcharts.report_district (id, district) VALUES (2, '珠三角2地区');
-INSERT INTO DjangoEcharts.report_district (id, district) VALUES (3, '粤东地区');
-INSERT INTO DjangoEcharts.report_district (id, district) VALUES (4, '粤西地区');
-INSERT INTO DjangoEcharts.report_district (id, district) VALUES (5, '粤北地区');
+INSERT INTO report_district (id, district) VALUES (1, '珠1');
+INSERT INTO report_district (id, district) VALUES (2, '珠2');
+INSERT INTO report_district (id, district) VALUES (3, '粤东');
+INSERT INTO report_district (id, district) VALUES (4, '粤西');
+INSERT INTO report_district (id, district) VALUES (5, '粤北');
